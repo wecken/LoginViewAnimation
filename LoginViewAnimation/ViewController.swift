ac87b26221d300f4afeb64e3f2cf020652ce3f05
Wecken <wecken.rainbow@gmail.com>
2020-11-07 12:31:38 +0900

    Initial Commit

---
+++ LoginViewAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginViewAnimation
+//
+//  Created by Hiroyuki Miura on 2020/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
