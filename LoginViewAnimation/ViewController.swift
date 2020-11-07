fa54e77dc0b3f5b68e421e3362c2c59b1fda4a04
Wecken <wecken.rainbow@gmail.com>
2020-11-07 15:28:21 +0900

    Added animation

--- LoginViewAnimation/ViewController.swift
+++ LoginViewAnimation/ViewController.swift
@@ -19,9 +19,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-
+        let resetButton = UIButton()
+        resetButton.translatesAutoresizingMaskIntoConstraints = false
+        resetButton.setTitle("Re-animate", for: .normal)
+        resetButton.addTarget(self, action: #selector(tapResetButton), for: .touchUpInside)
+        resetButton.setTitleColor(.systemBlue, for: .normal)
+        
+        view.addSubview(resetButton)
+        
+        NSLayoutConstraint.activate([
+            resetButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16),
+            resetButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    
+        
         setupLoginView()
         setupStackView()
+        prepareForAnimation()
         
         view.addSubview(stackView)
         
@@ -33,6 +47,9 @@ class ViewController: UIViewController {
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
+        
+        startAnimate()
+        
     }
     
     func setupLoginView() {
@@ -69,6 +86,44 @@ class ViewController: UIViewController {
         stackView.addArrangedSubview(imageView)
         stackView.addArrangedSubview(appleButton)
         stackView.addArrangedSubview(googleButton)
+    }
+    
+    func prepareForAnimation() {
+        titleLabel.layer.opacity = 0
+        imageView.layer.opacity = 0
+        appleButton.layer.opacity = 0
+        googleButton.layer.opacity = 0
+        
+        titleLabel.layer.setAffineTransform(CGAffineTransform.init(scaleX: 0.8, y: 0.8))
+        imageView.layer.setAffineTransform(CGAffineTransform.init(scaleX: 0.8, y: 0.8))
+        appleButton.layer.setAffineTransform(CGAffineTransform.init(translationX: 0, y: 30))
+        googleButton.layer.setAffineTransform(CGAffineTransform .init(translationX: 0, y: 60))
+        
+    }
+    
+    func startAnimate() {
+        UIView.animate(withDuration: 2.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 10, options: .curveEaseIn, animations: { [self] in
+            
+            self.titleLabel.layer.opacity = 1
+            self.titleLabel.layer.setAffineTransform(CGAffineTransform.identity)
+            self.imageView.layer.opacity = 1
+            self.imageView.layer.setAffineTransform(CGAffineTransform.identity)
+            
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 1, delay: 0.6, options: .curveEaseOut, animations: {
+            
+            self.appleButton.layer.opacity = 1
+            self.appleButton.layer.setAffineTransform(CGAffineTransform.identity)
+            self.googleButton.layer.opacity = 1
+            self.googleButton.layer.setAffineTransform(CGAffineTransform.identity)
+ 
+        }, completion: nil)
+    }
+    
+    @objc func tapResetButton() {
+        prepareForAnimation()
+        startAnimate()
     }
 
 
